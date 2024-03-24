044093be1a7ad8caba6ced6310046d417a7dc889
Jim Wei <jimwei47@live.ca>
2024-03-24 16:07:49 -0400

    added models

--- parcels/models.py
+++ parcels/models.py
@@ -5,3 +5,24 @@ from django.db import models
 # To be incorporated with a database like postgresql postgis
 
 # Comment on how you update a geospatial dataset when new data arrives. How would you handle conflicts or duplicate entries?
+
+class Parcels(models.Model):
+    id = models.PositiveBigIntegerField(primary_key=True, unique=True)
+    proj_name = models.CharField(max_length=255)
+    area = models.DecimalField(decimal_places=2)
+    status = models.CharField(max_length=255)
+    area_sf = models.DecimalField(decimal_places=2)
+    building_f = models.DecimalField(decimal_places=2)
+    height_m = models.DecimalField(decimal_places=2)
+    storey = models.PositiveSmallIntegerField()
+    gfa_sf = models.DecimalField(decimal_places=2)
+    density = models.DecimalField(decimal_places=2)
+    price = models.DecimalField(decimal_places=2)
+    units = models.PositiveSmallIntegerField()
+    sold_per = models.DecimalField(decimal_places=2)
+    type = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
+    
