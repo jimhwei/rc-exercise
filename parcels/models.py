12f8b5accb2b57dc1f9dfdfbb534717eb0d54b57
Jim Wei <jimwei47@live.ca>
2024-03-25 11:06:39 -0400

    model field update

--- parcels/models.py
+++ parcels/models.py
@@ -2,24 +2,24 @@ from django.db import models
 
 # Create your models here.
 
-# To be incorporated with a database like postgresql postgis
+# To be incorporated with a database like postgresql postgis TODO
 
 # Comment on how you update a geospatial dataset when new data arrives. How would you handle conflicts or duplicate entries?
 
 class Parcels(models.Model):
-    id = models.PositiveBigIntegerField(primary_key=True, unique=True)
+    id = models.CharField(primary_key=True, unique=True)
     proj_name = models.CharField(max_length=255)
-    area = models.DecimalField(decimal_places=2)
+    area = models.FloatField()
     status = models.CharField(max_length=255)
-    area_sf = models.DecimalField(decimal_places=2)
-    building_f = models.DecimalField(decimal_places=2)
-    height_m = models.DecimalField(decimal_places=2)
+    area_sf = models.FloatField()
+    building_f = models.FloatField()
+    height_m = models.FloatField()
     storey = models.PositiveSmallIntegerField()
-    gfa_sf = models.DecimalField(decimal_places=2)
-    density = models.DecimalField(decimal_places=2)
-    price = models.DecimalField(decimal_places=2)
+    gfa_sf = models.FloatField()
+    density = models.FloatField()
+    price = models.FloatField()
     units = models.PositiveSmallIntegerField()
-    sold_per = models.DecimalField(decimal_places=2)
+    sold_per = models.FloatField()
     type = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
     
